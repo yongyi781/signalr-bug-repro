7c33cf3ca42b60ea62f08e6195a7442fe3973ead
Yongyi <yongyi781@hotmail.com>
2016-08-24 17:34:13 -0400

    Add project files.

---
+++ src/SignalRBugRepro/Hubs/TestHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRBugRepro.Hubs
+{
+    public class TestHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            Clients.All.SendMessage(message);
+        }
+    }
+}
---
+++ src/SignalRBugRepro/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace SignalRBugRepro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://+:5000/")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
